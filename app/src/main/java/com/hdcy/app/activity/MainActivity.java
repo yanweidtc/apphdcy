eca12fab9aab5438e797f7b5196cb7999cc97b61
chiwenheng <826695301@qq.com>
2016-09-16 10:11:43 +0800

    增加登陆监听

--- app/src/main/java/com/hdcy/app/activity/MainActivity.java
+++ app/src/main/java/com/hdcy/app/activity/MainActivity.java
@@ -8,7 +8,10 @@ import android.widget.Toast;
 import com.hdcy.app.R;
 import com.hdcy.app.fragment.MainFragment;
 import com.hyphenate.EMCallBack;
+import com.hyphenate.EMConnectionListener;
+import com.hyphenate.EMError;
 import com.hyphenate.chat.EMClient;
+import com.hyphenate.util.NetUtils;
 
 import me.yokeyword.fragmentation.SupportActivity;
 import me.yokeyword.fragmentation.anim.DefaultHorizontalAnimator;
@@ -57,13 +60,13 @@ public class MainActivity extends SupportActivity {
             }
         });
 
+        //注册一个监听连接状态的listener
+        EMClient.getInstance().addConnectionListener(new MyConnectionListener());
+
 
         if (savedInstanceState == null) {
             loadRootFragment(R.id.fl_container_activity1, MainFragment.newInstance());
         }
-
-        //注册失败会抛出HyphenateException
-//        EMClient.getInstance().createAccount(username, pwd);//同步方法
 
     }
 
@@ -81,10 +84,37 @@ public class MainActivity extends SupportActivity {
 
     public  void showToast(String content){
         Toast.makeText(this,content,Toast.LENGTH_SHORT).show();
+    }
 
+    //实现ConnectionListener接口
+    private class MyConnectionListener implements EMConnectionListener {
+        @Override
+        public void onConnected() {
+        }
+        @Override
+        public void onDisconnected(final int error) {
+            runOnUiThread(new Runnable() {
 
-
-
+                @Override
+                public void run() {
+                    if(error == EMError.USER_REMOVED){
+                        // 显示帐号已经被移除
+                        showToast("显示帐号已经被移除");
+                    }else if (error == EMError.USER_LOGIN_ANOTHER_DEVICE) {
+                        // 显示帐号在其他设备登录
+                        showToast("显示帐号在其他设备登录");
+                    } else {
+                        if (NetUtils.hasNetwork(MainActivity.this)) {
+                            //连接不到聊天服务器
+                            showToast("连接不到聊天服务器");
+                        }else{
+                            //当前网络不可用，请检查网络设置
+                            showToast("当前网络不可用，请检查网络设置");
+                        }
+                    }
+                }
+            });
+        }
     }
 
 }
